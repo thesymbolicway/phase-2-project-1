f46546403e2383fa3674bab2e25b09aaef6b1adc
daniel.arisa@yahoo.com <daaniel.arisa@yahoo.com>
2022-09-14 23:43:27 -0500

    Form Beginning Stages

--- src/pages/personalPlaylistPage.jsx
+++ src/pages/personalPlaylistPage.jsx
@@ -1,7 +1,15 @@
+import React, {useState} from "react";
+
 function PersonalPlaylistPage() {
+    const [name, setName] = useState("")
+
     return (
-        <div>
-            <h1>Personal Playlist Page</h1>
+        <div className="new PersonalPlaylistPage">
+            <h2>Personal Playlist Page</h2>
+            <h2>New Song</h2>
+        <form>
+        <input onChange={(e)=>setName(e.target.value)} value={name} type="text" name="name" placeholder="Song Name" />
+        </form>
         </div>
     );
 }
